a72a534ee2280f4c9ef51603171efe6fdd200476
Janmejay Chatterjee <70024430+NotTheRightGuy@users.noreply.github.com>
2023-01-21 06:03:55 +0000

    Sync LeetCode submission - Duplicate Zeros (cpp)

--- problems/duplicate_zeros/solution.cpp
+++ problems/duplicate_zeros/solution.cpp
@@ -3,22 +3,16 @@ class Solution
 public:
     void duplicateZeros(vector<int> &arr)
     {
-        int Nsize = arr.size();
-        int countNumberofZeros = count(arr.begin(), arr.end(), 0);
-        vector<int> newVector(arr.size() + countNumberofZeros);
-        int indexOfNew{0}, indexOfOld{0};
-        while (indexOfOld < arr.size())
+        vector<int> newArr;
+        for (int i : arr)
         {
-            newVector[indexOfNew] = arr[indexOfOld];
-            if (arr[indexOfOld] == 0)
+            newArr.push_back(i);
+            if (i == 0)
             {
-                newVector[indexOfNew + 1] = 0;
-                indexOfNew++;
+                newArr.push_back(0);
             }
-            indexOfNew++;
-            indexOfOld++;
         }
-        arr = newVector;
-        arr.resize(Nsize);
+        newArr.resize(arr.size());
+        arr = newArr;
     }
 };
