2fb62cbb9df4667d66766c2f928ab968d71e41d6
Janmejay Chatterjee <70024430+NotTheRightGuy@users.noreply.github.com>
2023-02-08 13:21:52 +0000

    Sync LeetCode submission - Jump Game II (cpp)

---
+++ problems/jump_game_ii/solution.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int jump(vector<int> &nums)
+    {
+        int res = 0;
+        int left{0}, right{0};
+        int farthest = 0;
+        while (right < nums.size() - 1)
+        {
+            farthest = 0;
+            for (int i = left; i < right + 1; i++)
+            {
+                farthest = max(farthest, i + nums[i]);
+            }
+            left = right + 1;
+            right = farthest;
+            res++;
+        }
+        return res;
+    }
+};
