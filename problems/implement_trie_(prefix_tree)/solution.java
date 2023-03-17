a352ec5d24533bf285e0516cec42ef08760d27d1
Janmejay Chatterjee <70024430+NotTheRightGuy@users.noreply.github.com>
2023-03-17 11:02:17 +0000

    Sync LeetCode submission - Implement Trie (Prefix Tree) (java)

---
+++ problems/implement_trie_(prefix_tree)/solution.java
@@ -0,0 +1,53 @@
+class TrieNode {
+    boolean isWord;
+    TrieNode[] children;
+    
+    public TrieNode() {
+        isWord = false;
+        children = new TrieNode[26];
+    }
+}
+
+class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.isWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return node.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return true;
+    }
+}
