a464b36e2c8a17e510e8c43157100394803f615e
Janmejay Chatterjee <70024430+NotTheRightGuy@users.noreply.github.com>
2023-09-09 15:51:58 +0000

    Sync LeetCode submission - Single Number (cpp)

--- problems/single_number/solution.cpp
+++ problems/single_number/solution.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int, int> mp;
-    for (int i = 0; i < nums.size(); i++)
-    {
-        mp[nums[i]]++;
-    }
-    for (auto i : mp)
-    {
-        if (i.second == 1)
-        {
-            return i.first;
+        int temp = 0;
+        for(int i : nums){
+            temp ^= i;
         }
-    }
-    return -1;
+        return temp;
     }
 };
