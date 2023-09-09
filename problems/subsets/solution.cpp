506f724d72fb46b45c328f895abe2c0bb7a9c61f
Janmejay Chatterjee <70024430+NotTheRightGuy@users.noreply.github.com>
2023-09-09 16:04:39 +0000

    Sync LeetCode submission - Subsets (cpp)

--- problems/subsets/solution.cpp
+++ problems/subsets/solution.cpp
@@ -1,23 +1,25 @@
 class Solution
 {
 public:
-    void generate(vector<int> &nums, int i, vector<int> &subset,vector<vector<int>> &all)
+    vector<vector<int>> subsets(vector<int> &nums)
     {
-        if (i == nums.size()){
-            all.push_back(subset);
-            return;
+        vector<vector<int>> ans;
+        for (int i = 0; i < (1 << nums.size()); i++)
+        {
+            vector<int> temp;
+            int idx = 0;
+            int temp_i = i;
+            while (temp_i > 0)
+            {
+                if (temp_i & 1)
+                {
+                    temp.push_back(nums[idx]);
+                }
+                temp_i = temp_i >> 1;
+                idx++;
+            }
+            ans.push_back(temp);
         }
-        generate(nums, i + 1, subset,all);
-        subset.push_back(nums[i]);
-        generate(nums, i + 1, subset,all);
-        subset.pop_back();
-    }
-
-    vector<vector<int>> subsets(vector<int> nums)
-    {
-        vector<int> subset;
-        vector<vector<int>> all;
-        generate(nums, 0, subset,all);
-        return all;
+        return ans;
     }
 };
