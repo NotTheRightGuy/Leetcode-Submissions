ed0a60bbdb02ec2b6df27f7088ca7a2c4c811f71
Janmejay Chatterjee <70024430+NotTheRightGuy@users.noreply.github.com>
2023-09-17 05:36:12 +0000

    Sync LeetCode submission - Longest Repeating Character Replacement (cpp)

--- problems/longest_repeating_character_replacement/solution.cpp
+++ problems/longest_repeating_character_replacement/solution.cpp
@@ -3,21 +3,22 @@ class Solution
 public:
     int characterReplacement(string s, int k)
     {
-        map<char, int> mp;
-        int ans = INT_MIN;
+        map<int, int> mp;
         int left = 0;
-        int maxInWindow = 0;
+        int maxFreq = INT_MIN;
+        int ans = INT_MIN;
         for (int right = 0; right < s.length(); right++)
         {
             mp[s[right]]++;
-            maxInWindow = max(maxInWindow, mp[s[right]]);
+            maxFreq = max(maxFreq, mp[s[right]]);
 
-            if (right - left + 1 - maxInWindow > k)
+            while (right - left + 1 - maxFreq > k)
             {
                 mp[s[left]]--;
                 left++;
             }
-            ans = max(ans, right - left + 1);
+
+            ans = max(right - left + 1, ans);
         }
         return ans;
     }
