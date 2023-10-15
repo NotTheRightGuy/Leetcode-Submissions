22fee7436a8f1cf3137c2a5eed6c6b3932dc3ce8
Janmejay Chatterjee <70024430+NotTheRightGuy@users.noreply.github.com>
2023-10-15 14:47:30 +0000

    Sync LeetCode submission - Merge Intervals (cpp)

--- problems/merge_intervals/solution.cpp
+++ problems/merge_intervals/solution.cpp
@@ -1,23 +1,31 @@
-#define all(x) x.begin(), x.end()
-class Solution {
-public:
-    vector<vector<int>> merge(vector<vector<int>> &intervals)
-{
-    sort(all(intervals));
-    vector<vector<int>> merged;
 
-    for (int i = 0; i < intervals.size(); i++)
-    {
-        if (merged.empty() || merged.back()[1] < intervals[i][0])
-        {
-            vector<int> v = {intervals[i][0], intervals[i][1]};
-            merged.push_back(v);
-        }
-        else
-        {
-            merged.back()[1] = max(merged.back()[1], intervals[i][1]);
-        }
-    }
-    return merged;
+bool comp(vector<int> v1, vector<int> v2)
+{
+    return v1[0] < v2[0];
 }
+
+class Solution
+{
+public:
+    vector<vector<int>> merge(vector<vector<int>> intervals)
+    {
+        vector<vector<int>> answer;
+        sort(intervals.begin(), intervals.end(), comp);
+        pair<int, int> currentInterval = {intervals[0][0], intervals[0][1]};
+        for (int i = 1; i < intervals.size(); i++)
+        {
+            if (intervals[i][0] <= currentInterval.second)
+            {
+                currentInterval.second = max(currentInterval.second, intervals[i][1]);
+            }
+            else
+            {
+                answer.push_back({currentInterval.first, currentInterval.second});
+                currentInterval.first = intervals[i][0];
+                currentInterval.second = intervals[i][1];
+            }
+        }
+        answer.push_back({currentInterval.first, currentInterval.second});
+        return answer;
+    }
 };
