9e417d016d9b03aca1e908f5394106ab83175e4b
Janmejay Chatterjee <70024430+NotTheRightGuy@users.noreply.github.com>
2023-10-16 09:16:32 +0000

    Sync LeetCode submission - Balanced Binary Tree (cpp)

--- problems/balanced_binary_tree/solution.cpp
+++ problems/balanced_binary_tree/solution.cpp
@@ -1,26 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
+class Solution
+{
 public:
-    int heightOfTree(TreeNode* root){
-        if(!root) return 0;
-        int leftTree = heightOfTree(root->left);
-        if(leftTree == -1) return -1;
-        int rightTree = heightOfTree(root->right);
-        if(rightTree == -1) return -1;
-        if(abs(leftTree - rightTree) > 1) return -1;
-        return max(leftTree,rightTree) + 1;
+    int depth(TreeNode *root)
+    {
+        if (!root)
+            return 0;
+        int leftHeight = depth(root->left);
+        if (leftHeight == -1)
+            return -1;
+        int rightHeight = depth(root->right);
+        if (rightHeight == -1)
+            return -1;
+        if (abs(leftHeight - rightHeight) > 1)
+            return -1;
+        return max(leftHeight, rightHeight) + 1;
     }
-    bool isBalanced(TreeNode* root) {
-        return heightOfTree(root) != -1;
+    bool isBalanced(TreeNode *root)
+    {
+        return depth(root) != -1;
     }
 };
