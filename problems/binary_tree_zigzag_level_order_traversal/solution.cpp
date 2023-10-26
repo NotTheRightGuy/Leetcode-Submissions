54c4d2afba7be975f298159b0952a09d2c17edc9
Janmejay Chatterjee <70024430+NotTheRightGuy@users.noreply.github.com>
2023-10-26 07:37:01 +0000

    Sync LeetCode submission - Binary Tree Zigzag Level Order Traversal (cpp)

--- problems/binary_tree_zigzag_level_order_traversal/solution.cpp
+++ problems/binary_tree_zigzag_level_order_traversal/solution.cpp
@@ -1,37 +1,44 @@
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        
-        if (!root) return {};
-        
-        queue<TreeNode*> q;
-        vector<vector<int>> out;
-            
-        q.push(root);  
-        int level = 0; /* to alternate levels, or a bool variable */
-        
-        while (!q.empty()) {
-            
-            int sz = q.size();  
-            vector<int> curr(sz); 
-            
-            for (int i = 0; i < sz; i++) {
-                
-                TreeNode* tmp = q.front();
-                q.pop();
-                
-                if (level == 0) {
-                    curr[i] = tmp->val; // odd level, insert like 0, 1, 2, 3 etc. 
-                } else {
-                    curr[sz - i - 1] = tmp->val; /* even level insert from end. 3, 2, 1, 0. (sz - i - 1) to get the index from end */
+    vector<vector<int>> zigzagLevelOrder(TreeNode *root)
+    {
+        if (!root)
+            return {};
+        stack<TreeNode *> currentLevel;
+        stack<TreeNode *> nextLevel;
+        bool ltr = true;
+        currentLevel.push(root);
+        vector<vector<int>> ans;
+
+        while (!currentLevel.empty())
+        {
+            vector<int> currAns;
+            while (!currentLevel.empty())
+            {
+                TreeNode *curr = currentLevel.top();
+                currentLevel.pop();
+                currAns.push_back(curr->val);
+                if (ltr)
+                {
+                    if (curr->left)
+                        nextLevel.push(curr->left);
+                    if (curr->right)
+                        nextLevel.push(curr->right);
                 }
-                
-                if (tmp->left) q.push(tmp->left);
-                if (tmp->right) q.push(tmp->right);
+                else
+                {
+                    if (curr->right)
+                        nextLevel.push(curr->right);
+                    if (curr->left)
+                        nextLevel.push(curr->left);
+                }
             }
-            out.push_back(curr); // now push the level traversed to output vector
-            level = !level; // toggle the level using negation. or level == 0 ? level = 1 : level = 0;
+            ans.push_back(currAns);
+            currentLevel = nextLevel;
+            nextLevel = stack<TreeNode *>();
+            ltr = !ltr;
         }
-        return out;
+        return ans;
     }
 };
